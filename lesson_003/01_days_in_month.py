d71bf7a6370caff32b66886e17249582f74f685c
Ivan <hobbeat@mail.ru>
2021-06-26 11:24:28 +0700

    lesson 3, task 01

--- lesson_003/01_days_in_month.py
+++ lesson_003/01_days_in_month.py
@@ -11,4 +11,13 @@ user_input = input("Введите, пожалуйста, номер месяц�
 month = int(user_input)
 print('Вы ввели', month)
 
-# TODO здесь ваш код
+if 0 < month < 13:
+    if month == 2:
+        days = 28
+    elif month in [1, 3, 5, 7, 8, 10, 12]:
+        days = 31
+    else:
+        days = 30
+    print(days)
+else:
+    print('Некорректный номер месяца!')
